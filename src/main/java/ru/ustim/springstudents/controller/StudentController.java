be2e6afde7ffdcb95fc626d4c833f3cabc0d653d
Ustim <ustimenkodenis@mail.ru>
2024-05-12 18:43:02 +0300

    feat: CRUD готов

--- src/main/java/ru/ustim/springstudents/controller/StudentController.java
+++ src/main/java/ru/ustim/springstudents/controller/StudentController.java
@@ -2,9 +2,7 @@ package ru.ustim.springstudents.controller;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.ustim.springstudents.model.Student;
 import ru.ustim.springstudents.service.StudentService;
 
@@ -20,5 +18,21 @@ public class StudentController {
     @GetMapping
     public List<Student> findAllStudent() {
         return service.findAllStudent();
+    }
+    @PostMapping("save_student")
+    public Student saveStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+    @GetMapping("/{email}")
+    public Student findByEmail(@PathVariable String email) {
+        return service.findByEmail(email);
+    }
+    @PutMapping("update_student")
+    public Student updateStudent(@RequestBody Student student) {
+        return service.updateStudent(student);
+    }
+    @DeleteMapping("delete_student/{email}")
+    public void deleteStudent(@PathVariable String email) {
+        service.deleteStudent(email);
     }
 }
--- src/main/java/ru/ustim/springstudents/service/impl/InMemoryStudentServiceImpl.java
+++ src/main/java/ru/ustim/springstudents/service/impl/InMemoryStudentServiceImpl.java
@@ -1,37 +1,42 @@
 package ru.ustim.springstudents.service.impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.ustim.springstudents.model.Student;
+import ru.ustim.springstudents.repository.InMemoryStudentDAO;
 import ru.ustim.springstudents.service.StudentService;
 
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class InMemoryStudentServiceImpl implements StudentService {
+
+    private final InMemoryStudentDAO repository;
 
     @Override
     public List<Student> findAllStudent() {
-        return null;
+        return repository.findAllStudent();
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return null;
+        return repository.saveStudent(student);
     }
 
     @Override
     public Student findByEmail(String email) {
-        return null;
+        return repository.findByEmail(email);
     }
 
     @Override
     public Student updateStudent(Student student) {
-        return null;
+        return repository.updateStudent(student);
     }
 
     @Override
     public void deleteStudent(String email) {
-
+        repository.deleteStudent(email);
     }
 
 }
